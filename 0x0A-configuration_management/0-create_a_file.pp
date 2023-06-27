98b00af8ed5b7ae924eb86771fdc529e0a97f476
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-27 16:17:25 +0000

    added the task one

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,12 @@
 # This create folder and file
-
 file {'/tmp':
     ensure => directory,
 }
 
 file {'/tmp/school':
-    ensure => file,
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
